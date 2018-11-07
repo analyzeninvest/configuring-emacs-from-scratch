d7beca1bda76bcbe617b11d1cfdaa2b980ff8098
aritra-b-11 <aritrabhattacharjee12@gmail.com>
2018-11-08 00:06:36 +0530

    yasnippet with company mode

---
+++ elpa/yasnippet-0.13.0/yasnippet-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from yasnippet.el
+(define-package "yasnippet" "0.13.0" "Yet another snippet extension for Emacs." '((cl-lib "0.5")) :url "http://github.com/joaotavora/yasnippet" :keywords '("convenience" "emulation"))
