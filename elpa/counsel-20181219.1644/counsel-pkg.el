c13bcb098baccb712c6e3ceda478c1bf28a7ffaa
aritra-b-11 <aritrabhattacharjee12@gmail.com>
2019-01-02 00:43:08 +0530

    removed some old packages

--- elpa/counsel-20181219.1644/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20181219.1644" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "7ff7d1d60441c84c9bbb0cabe078d72aca04a8eb" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
--- elpa/git-commit-20181219.1846/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20181219.1846" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "f6e04ddd7e1c3d4bbe85581f77745665b3be67ea" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
